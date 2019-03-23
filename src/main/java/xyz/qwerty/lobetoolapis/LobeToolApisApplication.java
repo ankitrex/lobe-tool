ebab47ec3ce90be1863c502931922a78fd301433
ankitrex <theankitgajra@gmail.com>
2019-03-23 12:59:44 +0530

    Initial commit

---
+++ src/main/java/xyz/qwerty/lobetoolapis/LobeToolApisApplication.java
@@ -0,0 +1,13 @@
+package xyz.qwerty.lobetoolapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobeToolApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobeToolApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/qwerty/lobetoolapis/LobeToolApisApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.qwerty.lobetoolapis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LobeToolApisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
